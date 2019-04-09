ae197eae2842f86343f349f00439b6d70b029482
buttsj <buttsj@wit.edu>
2019-04-08 20:22:54 -0400

    Merge branch 'hashMap' of https://github.com/Jideen/Wentworth-GPS into #9/layout-skinning
    # Conflicts:
    #	.idea/caches/build_file_checksums.ser

--- app/src/main/java/edu/wit/myapplication/mapActivity.java
+++ app/src/main/java/edu/wit/myapplication/mapActivity.java
@@ -1,14 +1,22 @@
 package edu.wit.myapplication;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.TextView;
 
-public class mapActivity extends AppCompatActivity {
-    //TODO: http://graphonline.ru/en/?graph=XSUinfwLhHwsARkb
+public class MapActivity extends AppCompatActivity {
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_map);
+        setContentView(R.layout.activity_map2);
+        final TextView text = findViewById(R.id.textView2);
+
+        String source = getIntent().getExtras().getString("Source");
+        String dest = getIntent().getExtras().getString("Target");
+        Pathfinder p = new Pathfinder();
+        text.setText(p.run(source,dest));
     }
 
 }
