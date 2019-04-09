4c794d53882d236274c30758857555aa1a2881ef
buttsj <buttsj@wit.edu>
2019-04-09 13:53:50 -0400

    update branch

--- app/src/main/java/edu/wit/myapplication/MapActivity.java
+++ app/src/main/java/edu/wit/myapplication/MapActivity.java
@@ -11,18 +11,20 @@ public class mapActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_map2);
+        setContentView(new DrawView(this));
         final TextView text = findViewById(R.id.textView2);
 
         String source = getIntent().getExtras().getString("Source");
         String dest = getIntent().getExtras().getString("Dest");
         Pathfinder p = new Pathfinder();
         StringBuilder s = new StringBuilder();
+        int len = p.run(source,dest).length;
         String[] strArray = p.run(source,dest);
-        for(int i=0;i<strArray.length; i++){
+        for(int i=0;i<len; i++){
             s.append(strArray[i]);
-            s.append(", ");
+            //s.append(", ");
         }
-        text.setText(s.toString());
+//        text.setText(s.toString());
     }
 
 }
